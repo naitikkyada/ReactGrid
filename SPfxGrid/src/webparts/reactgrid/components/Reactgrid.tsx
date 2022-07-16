421b62dd1188b4554855ec1b8013ad26b079b0d3
naitikkyada <59310899+naitikkyada@users.noreply.github.com>
2022-07-16 13:16:17 +0530

    Added Grid
    Added Grid

--- SPfxGrid/src/webparts/reactgrid/components/Reactgrid.tsx
+++ SPfxGrid/src/webparts/reactgrid/components/Reactgrid.tsx
@@ -2,23 +2,134 @@ import * as React from 'react';
 import styles from './Reactgrid.module.scss';
 import { IReactgridProps } from './IReactgridProps';
 import { escape } from '@microsoft/sp-lodash-subset';
+import BootstrapTable from 'react-bootstrap-table-next';
+import ToolkitProvider from 'react-bootstrap-table2-toolkit';
+import Tabel from './Tabel';
+
+export interface ICustomDatatableState {
+  selectedRows: any;
+}
+const audittaskTableColumns = [
+  {
+    text: "Name",
+    dataField: 'Name',
+    sort: true
+  },
+  {
+    text: 'Address',
+    dataField: 'Address',
+    sort: true
+  },
+  {
+    text: 'Email',
+    dataField: 'Email',
+    sort: true
+  },
+  {
+    text: 'Phone',
+    dataField: 'Phone',
+    sort: true
+  },
+  {
+    text: 'StartDate',
+    dataField: 'StartDate',
+    sort: true
+  },
+  {
+    text: 'EndDate',
+    dataField: 'EndDate',
+    sort: true
+  },
+];
+
+const Documenttabel=[
+  {
+    "Name" : "Test User",
+    "Address":"Street 235 Paradise tower, S.G Highway Ahmedabad",
+    "Email":"abc@gmail.com ",
+    "Phone":"9904587962",
+    "StartDate":"1 june",
+    "EndDate":"30 june",
+  },
+  {
+    "Name" : "Test User",
+    "Address":"Street 235 Paradise tower, S.G Highway Ahmedabad",
+    "Email":"abc@gmail.com ",
+    "Phone":"9904587962",
+    "StartDate":"1 june",
+    "EndDate":"30 june",
+  },
+  {
+    "Name" : "Test User",
+    "Address":"Street 235 Paradise tower, S.G Highway Ahmedabad",
+    "Email":"abc@gmail.com ",
+    "Phone":"9904587962",
+    "StartDate":"1 june",
+    "EndDate":"30 june",
+  },
+  {
+    "Name" : "Test User",
+    "Address":"Street 235 Paradise tower, S.G Highway Ahmedabad",
+    "Email":"abc@gmail.com ",
+    "Phone":"9904587962",
+    "StartDate":"1 june",
+    "EndDate":"30 june",
+  },
+  {
+    "Name" : "Test User",
+    "Address":"Street 235 Paradise tower, S.G Highway Ahmedabad",
+    "Email":"abc@gmail.com ",
+    "Phone":"9904587962",
+    "StartDate":"1 june",
+    "EndDate":"30 june",
+  },
+  {
+    "Name" : "Test User",
+    "Address":"Street 235 Paradise tower, S.G Highway Ahmedabad",
+    "Email":"abc@gmail.com ",
+    "Phone":"9904587962",
+    "StartDate":"1 june",
+    "EndDate":"30 june",
+  },
+  {
+    "Name" : "Test User",
+    "Address":"Street 235 Paradise tower, S.G Highway Ahmedabad",
+    "Email":"abc@gmail.com ",
+    "Phone":"9904587962",
+    "StartDate":"1 june",
+    "EndDate":"30 june",
+  },
+  {
+    "Name" : "Test User",
+    "Address":"Street 235 Paradise tower, S.G Highway Ahmedabad",
+    "Email":"abc@gmail.com ",
+    "Phone":"9904587962",
+    "StartDate":"1 june",
+    "EndDate":"30 june",
+  },
+  {
+    "Name" : "Test User",
+    "Address":"Street 235 Paradise tower, S.G Highway Ahmedabad",
+    "Email":"abc@gmail.com ",
+    "Phone":"9904587962",
+    "StartDate":"1 june",
+    "EndDate":"30 june",
+  },
+  {
+    "Name" : "Test User",
+    "Address":"Street 235 Paradise tower, S.G Highway Ahmedabad",
+    "Email":"abc@gmail.com ",
+    "Phone":"9904587962",
+    "StartDate":"1 june",
+    "EndDate":"30 june",
+  },
+];
 
 export default class Reactgrid extends React.Component<IReactgridProps, {}> {
   public render(): React.ReactElement<IReactgridProps> {
     return (
-      <div className={ styles.reactgrid }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>Welcome to SharePoint!</span>
-              <p className={ styles.subTitle }>Customize SharePoint experiences using Web Parts.</p>
-              <p className={ styles.description }>{escape(this.props.description)}</p>
-              <a href="https://aka.ms/spfx" className={ styles.button }>
-                <span className={ styles.label }>Learn more</span>
-              </a>
-            </div>
-          </div>
-        </div>
+      <div>
+          <Tabel keyField={"TaskID"} TableDataAndColumns={{ columns: audittaskTableColumns, rows: Documenttabel }}></Tabel>
       </div>
     );
   }
