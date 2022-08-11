ca903bd060c936a7a85114f36aa9aa97156b4bc9
naitikkyada <59310899+naitikkyada@users.noreply.github.com>
2022-08-12 00:16:22 +0530

    Modern Script Editor
    Modern Script Editor initial commit

---
+++ ModernScriptEditor/src/webparts/modernScriptEditor/loc/mystrings.d.ts
@@ -0,0 +1,14 @@
+declare interface IModernScriptEditorWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+}
+
+declare module 'ModernScriptEditorWebPartStrings' {
+  const strings: IModernScriptEditorWebPartStrings;
+  export = strings;
+}
